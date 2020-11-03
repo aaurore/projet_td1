c0dd443d9d3ae7cf90b4feb4cd692d6c4f045297
Your Name <you@example.com>
2020-11-03 10:44:35 +0100

    ajout du fichier csv

--- projet1.py
+++ projet1.py
@@ -2,6 +2,8 @@ import csv
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+import statistics as st
+
 
 df = pd.read_csv("EIVP/EIVP_KM.csv", sep=';') #la fichier devient un dataframe, on l'affiche sans les ;
 #print(df) #on affiche le tableau
@@ -26,11 +28,19 @@ def nouveau_temps(liste,start_date, end_date):
 
 x=df['sent_at'] #on extrait la colonne temps
 y=df['noise'] #on extrait la colonne noise
-plt.plot(x,y)
+p1=plt.plot(x,y)
+a=st.mean(y)
+z=[a]*len(x)
+p2=plt.plot(x,z)
 plt.show()
 #print(x)
 
 
+# p1=plt.plot(x,np.sin(x),marker='o')
+# p2=plt.plot(x,np.cos(x),marker='v')
+# plt.title("Fonctions trigonometriques")  # Problemes avec accents (plot_directive) !
+# plt.legend([p1, p2], ["Sinus", "Cosinus"])
+# plt.show()
 
 
 #def graph_var_fonct_temps(variable):
